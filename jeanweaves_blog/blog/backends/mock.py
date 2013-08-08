6f2f9ad357bcef601cf37a791b68da04ed3939e9
Steven Cummings <estebistec@gmail.com>
2013-08-07 22:59:57 -0500

    Add more posts to mock blog posts backend

--- jeanweaves_blog/blog/backends/mock.py
+++ jeanweaves_blog/blog/backends/mock.py
@@ -14,5 +14,15 @@ class Backend(object):
           title=u'Test',
           time=parse(u'2013-08-07 01:27:10 GMT'),
           body=u'<p>Test</p>'
+        ),
+        Post(
+          title=u'List test',
+          time=parse(u'2013-08-06 01:27:10 GMT'),
+          body=u'<ul><li>A</li><li>B</li><li>C</li></ul>'
+        ),
+        Post(
+          title=u'Test',
+          time=parse(u'2013-08-05 01:27:10 GMT'),
+          body=u'<p>Test</p>'
         )
     ]
