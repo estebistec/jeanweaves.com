39b821398e8753113ae6d3ded76ff67039fba6a0
Steven Cummings <estebistec@gmail.com>
2013-12-29 21:30:39 -0600

    Update gallery with new photos, updated labels

--- jeanweaves_galleries/galleries.py
+++ jeanweaves_galleries/galleries.py
@@ -15,7 +15,7 @@ index = [
     Photo(title=_('Damask'), src_path='img/damask/512/damask1.jpg', detail_src_path=None, alt=_('Damask 1'), link_url=reverse_lazy('damask')),
     Photo(title=_('Placemats and Napkins'), src_path='img/placemats-and-napkins/512/napkins.jpg', detail_src_path=None, alt=_('Napkins'), link_url=reverse_lazy('placemats_and_napkins')),
     Photo(title=_('Runners'), src_path='img/runners/512/runner2.jpg', detail_src_path=None, alt=_('Runner 2'), link_url=reverse_lazy('runners')),
-    Photo(title=_('Scarves'), src_path='img/scarves/512/scarf3.jpg', detail_src_path=None, alt=_('Scarf 3'), link_url=reverse_lazy('scarves')),
+    Photo(title=_('Scarves'), src_path='img/scarves/512/scarf-thumbail.jpg', detail_src_path=None, alt=_('Scarf 3'), link_url=reverse_lazy('scarves')),
     Photo(title=_('Throws'), src_path='img/throws/512/throw2.jpg', detail_src_path=None, alt=_('Throw 2'), link_url=reverse_lazy('throws')),
     Photo(title=_('Animals'), src_path='img/animals/512/cow.jpg', detail_src_path=None, alt=_('Cow'), link_url=reverse_lazy('animals')),
 ]
@@ -28,10 +28,10 @@ animals = [
 
 damask = [
     Photo(title=_("2nd Place Midwest Weavers Members' Exhibit-People of the South Wind and The Handwoven Weaving for the Home Award of Excellence"), src_path='img/damask/512/damask1.jpg', detail_src_path=None, alt=_("2nd Place Midwest Weavers Members' Exhibit-People of the South Wind and The Handwoven Weaving for the Home Award of Excellence"), link_url=None),
-    Photo(title=_('Damask 2'), src_path='img/damask/512/damask2.jpg', detail_src_path=None, alt=_('Damask 2'), link_url=None),
-    Photo(title=_('Damask 3'), src_path='img/damask/512/damask3.jpg', detail_src_path=None, alt=_('Damask 3'), link_url=None),
-    Photo(title=_('Damask 4'), src_path='img/damask/512/damask4.jpg', detail_src_path=None, alt=_('Damask 4'), link_url=None),
-    Photo(title=_('Damask 5'), src_path='img/damask/512/damask5.jpg', detail_src_path=None, alt=_('Damask 5'), link_url=None),
+    Photo(title=_('Star and Snowflake Damask in Natural'), src_path='img/damask/512/damask2.jpg', detail_src_path=None, alt=_('Star and Snowflake Damask in Natural'), link_url=None),
+    Photo(title=_('Star and Snowflake Damask in Green and White'), src_path='img/damask/512/damask4.jpg', detail_src_path=None, alt=_('Star and Snowflake Damask in Green and White'), link_url=None),
+    Photo(title=_('Star and Rose Damask in Red and White'), src_path='img/damask/512/damask5.jpg', detail_src_path=None, alt=_('Star and Rose Damask in Red and White'), link_url=None),
+    Photo(title=_('Damask Green on Green'), src_path='img/damask/512/damask-green-on-green.jpg', detail_src_path=None, alt=_('Damask Green on Green'), link_url=None),
 ]
 
 placemats_and_napkins = [
@@ -52,13 +52,14 @@ placemats_and_napkins = [
 
 runners = [
     Photo(title=_('Overshot holiday runner'), src_path='img/runners/512/Runner_Overshot_Holiday.jpg', detail_src_path='img/runners/512/Runner_Overshot_Holiday_Detail.jpg', alt=_('Overshot holiday runner'), link_url=None),
-    Photo(title=_('Runner 1'), src_path='img/runners/512/runner1.jpg', detail_src_path=None, alt=_('Runner 1'), link_url=None),
-    Photo(title=_('Runner 2'), src_path='img/runners/512/runner2.jpg', detail_src_path=None, alt=_('Runner 2'), link_url=None),
-    Photo(title=_('Runner 3'), src_path='img/runners/512/runner3.jpg', detail_src_path=None, alt=_('Runner 3'), link_url=None),
-    Photo(title=_('Runner 4'), src_path='img/runners/512/runner4.jpg', detail_src_path=None, alt=_('Runner 4'), link_url=None),
-    Photo(title=_('Runner 5'), src_path='img/runners/512/runner5.jpg', detail_src_path=None, alt=_('Runner 5'), link_url=None),
-    Photo(title=_('Runner 6'), src_path='img/runners/512/runner6.jpg', detail_src_path=None, alt=_('Runner 6'), link_url=None),
-    Photo(title=_('Runner 7'), src_path='img/runners/512/runner7.jpg', detail_src_path=None, alt=_('Runner 7'), link_url=None),
+    Photo(title=_('Green Blend Cotton Runner'), src_path='img/runners/512/runner1.jpg', detail_src_path=None, alt=_('Green Blend Cotton Runner'), link_url=None),
+    Photo(title=_('Natural Runner with Red Border'), src_path='img/runners/512/runner2.jpg', detail_src_path=None, alt=_('Natural Runner with Red Border'), link_url=None),
+    Photo(title=_('Green Blend Cotton Runner Detail'), src_path='img/runners/512/runner3.jpg', detail_src_path=None, alt=_('Green Blend Cotton Runner Detail'), link_url=None),
+    Photo(title=_('Blue and White Runner'), src_path='img/runners/512/runner4.jpg', detail_src_path=None, alt=_('Blue and White Runner'), link_url=None),
+    Photo(title=_('Blue and White Runner Detail'), src_path='img/runners/512/runner5.jpg', detail_src_path=None, alt=_('Blue and White Runner Detail'), link_url=None),
+    Photo(title=_('Double Weave Block Runner'), src_path='img/runners/512/runner6.jpg', detail_src_path=None, alt=_('Double Weave Block Runner'), link_url=None),
+    Photo(title=_('Double Weave Block Runner Detail'), src_path='img/runners/512/runner7.jpg', detail_src_path=None, alt=_('Double Weave Block Runner Detail'), link_url=None),
+    Photo(title=_('Forest Green Table Runner'), src_path='img/runners/512/forest-green-table-runner.jpg', detail_src_path=None, alt=_('Forest Green Table Runner'), link_url=None),
 ]
 
 scarves = [
@@ -67,15 +68,17 @@ scarves = [
     Photo(title=_("Sarah's scarf"), src_path='img/scarves/512/scarf-sarah.jpg', detail_src_path=None, alt=_("Sarah's scarf"), link_url=None),
     Photo(title=_('Scarf 1'), src_path='img/scarves/512/scarf1.jpg', detail_src_path=None, alt=_('Scarf 1'), link_url=None),
     Photo(title=_('Scarf 2'), src_path='img/scarves/512/scarf2.jpg', detail_src_path=None, alt=_('Scarf 2'), link_url=None),
-    Photo(title=_('Scarf 3'), src_path='img/scarves/512/scarf3.jpg', detail_src_path=None, alt=_('Scarf 3'), link_url=None),
-    Photo(title=_('Scarf 4'), src_path='img/scarves/512/scarf4.jpg', detail_src_path=None, alt=_('Scarf 4'), link_url=None),
-    Photo(title=_('Evergreen and Navy Scarf'), src_path='img/scarves/512/evergreen-and-navy-scarf.jpg', detail_src_path='img/scarves/512/evergreen-and-navy-scarf-detail.jpg', alt=_('Evergreen and Navy Scarf'), link_url=None),
-    Photo(title=_('Navy Variegated Scarf'), src_path='img/scarves/512/navy-variegated-scarf.jpg', detail_src_path='img/scarves/512/navy-variegated-scarf-detail.jpg', alt=_('Navy Variegated Scarf'), link_url=None),
+    Photo(title=_('Bamboo and Cotton Scarf in Blue and Lavender'), src_path='img/scarves/512/bamboo-and-cotton-scarf-in-blue-and-lavender.jpg', detail_src_path=None, alt=_('Bamboo and Cotton Scarf in Blue and Lavender'), link_url=None),
+    Photo(title=_('Mixed Berry Bamboo and Cotton Scarf'), src_path='img/scarves/512/mixed-berry-bamboo-and-cotton-scarf.jpg', detail_src_path=None, alt=_('Mixed Berry Bamboo and Cotton Scarf'), link_url=None),
+    Photo(title=_('Rust Brown Tencel Scarf 1'), src_path='img/scarves/512/rust-brown-tencel-scarf-1.jpg', detail_src_path=None, alt=_('Rust Brown Tencel Scarf 1'), link_url=None),
+    Photo(title=_('Rust Brown Tencel Scarf 2'), src_path='img/scarves/512/rust-brown-tencel-scarf-2.jpg', detail_src_path=None, alt=_('Rust Brown Tencel Scarf 2'), link_url=None),
+    Photo(title=_('Violet Navy Tencel Scarf'), src_path='img/scarves/512/violet-navy-tencel-scarf.jpg', detail_src_path=None, alt=_('Violet Navy Tencel Scarf'), link_url=None),
+    Photo(title=_('Violet Tencel Scarf'), src_path='img/scarves/512/violet-tencel-scarf.jpg', detail_src_path=None, alt=_('Violet Tencel Scarf'), link_url=None),
 ]
 
 throws = [
-    Photo(title=_('Throw 1'), src_path='img/throws/512/throw1.jpg', detail_src_path=None, alt=_('Throw 1'), link_url=None),
-    Photo(title=_('Throw 2'), src_path='img/throws/512/throw2.jpg', detail_src_path=None, alt=_('Throw 2'), link_url=None),
+    Photo(title=_('Summer and Winter Throws in Wool'), src_path='img/throws/512/throw1.jpg', detail_src_path=None, alt=_('Summer and Winter Throws in Wool'), link_url=None),
+    Photo(title=_('Overshot Throw, Early American Coverlet Design'), src_path='img/throws/512/throw2.jpg', detail_src_path=None, alt=_('Overshot Throw, Early American Coverlet Design'), link_url=None),
 ]
 
 towels = [
@@ -91,16 +94,16 @@ towels = [
     Photo(title=_('Blue huck towel'), src_path='img/towels/512/Towel_Blue_Huck.jpg', detail_src_path='img/towels/512/Towel_Blue_Huck_Detail.jpg', alt=_('Blue huck towel'), link_url=None),
     Photo(title=_('Raspberry huck towel'), src_path='img/towels/512/Towel_Raspberry_Huck.jpg', detail_src_path='img/towels/512/Towel_Raspberry_Huck_Detail.jpg', alt=_('Raspberry huck towel'), link_url=None),
     Photo(title=_('Rose blue towel'), src_path='img/towels/512/Towel_Rose_Blue_Mult.JPG', detail_src_path='img/towels/512/Towel_Rose_Blue_Mult_Detail.JPG', alt=_('Rose blue towel'), link_url=None),
-    Photo(title=_('Towel 1'), src_path='img/towels/512/towel1.jpg', detail_src_path=None, alt=_('Towel 1'), link_url=None),
-    Photo(title=_('Towel 2'), src_path='img/towels/512/towel2.jpg', detail_src_path=None, alt=_('Towel 2'), link_url=None),
-    Photo(title=_('Towel 3'), src_path='img/towels/512/towel3.jpg', detail_src_path=None, alt=_('Towel 3'), link_url=None),
-    Photo(title=_('Towel 4'), src_path='img/towels/512/towel4.jpg', detail_src_path=None, alt=_('Towel 4'), link_url=None),
-    Photo(title=_('Towel 5'), src_path='img/towels/512/towel5.jpg', detail_src_path=None, alt=_('Towel 5'), link_url=None),
-    Photo(title=_('Towel 6'), src_path='img/towels/512/towel6.jpg', detail_src_path=None, alt=_('Towel 6'), link_url=None),
-    Photo(title=_('Towel 7'), src_path='img/towels/512/towel7.jpg', detail_src_path=None, alt=_('Towel 7'), link_url=None),
-    Photo(title=_('Towel 8'), src_path='img/towels/512/towel8.jpg', detail_src_path=None, alt=_('Towel 8'), link_url=None),
-    Photo(title=_('Towel 9'), src_path='img/towels/512/towel9.jpg', detail_src_path=None, alt=_('Towel 9'), link_url=None),
-    Photo(title=_('Towel 10'), src_path='img/towels/512/towel10.jpg', detail_src_path=None, alt=_('Towel 10'), link_url=None),
-    Photo(title=_('Towel 11'), src_path='img/towels/512/towel11.jpg', detail_src_path=None, alt=_('Towel 11'), link_url=None),
+    Photo(title=_('Random Rose Kitchen Towel'), src_path='img/towels/512/towel1.jpg', detail_src_path=None, alt=_('Random Rose Kitchen Towel'), link_url=None),
+    Photo(title=_('Bread Towel in Naturally Colored Cotton'), src_path='img/towels/512/towel2.jpg', detail_src_path=None, alt=_('Bread Towel in Naturally Colored Cotton'), link_url=None),
+    Photo(title=_('Rose Stripe Kitchen Towel'), src_path='img/towels/512/towel3.jpg', detail_src_path=None, alt=_('Rose Stripe Kitchen Towel'), link_url=None),
+    Photo(title=_('Trio of Towels'), src_path='img/towels/512/towel4.jpg', detail_src_path=None, alt=_('Trio of Towels'), link_url=None),
+    Photo(title=_('Natural Rose Kitchen Towel'), src_path='img/towels/512/towel5.jpg', detail_src_path=None, alt=_('Natural Rose Kitchen Towel'), link_url=None),
+    Photo(title=_('Blue Blend Towels'), src_path='img/towels/512/towel6.jpg', detail_src_path=None, alt=_('Blue Blend Towels'), link_url=None),
+    Photo(title=_('Natural and Tomato Kitchen Towel'), src_path='img/towels/512/towel7.jpg', detail_src_path=None, alt=_('Natural and Tomato Kitchen Towel'), link_url=None),
+    Photo(title=_('Blue Blend and Rose Towel'), src_path='img/towels/512/towel10.jpg', detail_src_path=None, alt=_('Blue Blend and Rose Towel'), link_url=None),
     Photo(title=_('Towel 12'), src_path='img/towels/512/towel12.jpg', detail_src_path=None, alt=_('Towel 12'), link_url=None),
+    Photo(title=_('Towel Blue Blend Heavy Navy'), src_path='img/towels/512/towel-blue-blend-heavy-navy.jpg', detail_src_path=None, alt=_('Towel Blue Blend Heavy Navy'), link_url=None),
+    Photo(title=_('Towel Salsa Greens'), src_path='img/towels/512/towel-salsa-greens.jpg', detail_src_path=None, alt=_('Towel Salsa Greens'), link_url=None),
+    Photo(title=_('Towel Salsa Salad Both Sides'), src_path='img/towels/512/towel-salsa-salad-both-sides.jpg', detail_src_path=None, alt=_('Towel Salsa Salad Both Sides'), link_url=None),
 ]
